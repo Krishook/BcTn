7b47f22215b59dacd92e49e5c77b01d67f2dcefc
Krishook <christophe.boucquey@gmail.com>
2024-12-06 18:38:00 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,64 +7,6 @@
         <link rel="icon" type="image/x-icon" href="https://my-web-sites-app-fc79c0bb9acc.herokuapp.com/images/falco.ico">
        	<link rel="stylesheet" href="https://my-web-sites-app-fc79c0bb9acc.herokuapp.com/style.css">
 
-<?php
-$motDePasse = "Poi-987";
-
-if (!isset($_POST['password']) || $_POST['password'] !== $motDePasse) {
-    echo '
-        <style>
-            body {
-                font-family: Arial, sans-serif;
-                background-color: #f4f4f4;
-                margin: 0;
-                padding: 0;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                height: 100vh;
-            }
-            .form-container {
-                background-color: #fff;
-                padding: 20px;
-                border-radius: 8px;
-                box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-                text-align: center;
-                width: 300px;
-            }
-            .form-container h2 {
-                margin-bottom: 20px;
-                color: #333;
-            }
-            .form-container label {
-                display: block;
-                font-weight: bold;
-                margin-bottom: 8px;
-                color: #555;
-            }
-            .form-container input[type="password"] {
-                width: 100%;
-                padding: 10px;
-                margin-bottom: 20px;
-                border: 1px solid #ccc;
-                border-radius: 4px;
-                box-sizing: border-box;
-                font-size: 14px;
-            }
-            .form-container button {
-                width: 100%;
-                padding: 10px;
-                background-color: #007BFF;
-                color: #fff;
-                border: none;
-                border-radius: 4px;
-                font-size: 16px;
-                cursor: pointer;
-            }
-            .form-container button:hover {
-                background-color: #0056b3;
-            }
-        </style>
-
     </head>
 
 <body>
@@ -79,11 +21,7 @@ if (!isset($_POST['password']) || $_POST['password'] !== $motDePasse) {
     exit;
 }
 
-// Le mot de passe est correct, continuer l'exécution du script...
-?>
-
-
-<!-- <?php
+<?php
 $motDePasse = "Poi-987";
 if (!isset($_POST['password']) || $_POST['password'] !== $motDePasse) {
     echo '<form method="POST">
@@ -93,7 +31,7 @@ if (!isset($_POST['password']) || $_POST['password'] !== $motDePasse) {
           </form>';
     exit;
 }
-?> -->
+?>
     
 <center>
 
