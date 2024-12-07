2389d84b4021174053f9f2254c0d2371717a6845
Krishook <christophe.boucquey@gmail.com>
2024-12-07 09:32:37 +0100

    Update index.php

--- index.php
+++ index.php
@@ -78,7 +78,7 @@ if (!isset($_POST['password']) || trim($_POST['password']) !== $motDePasse) {
 }
 
 // Redirection après validation réussie
-header("Location: https://my-web-sites-app-fc79c0bb9acc.herokuapp.com/home.php");
+header("Location: https://my-web-sites-app-fc79c0bb9acc.herokuapp.com/home.html");
 exit;
 ?>
 
